e5193986dee66a53e88b10b39d760c3ab4f89022
kr!shna <ketan.sahane@techprimelab.com>
2017-05-19 20:46:46 +0530

    changes in static messages and fields

--- src/client/app/cnext/framework/model/q-cardview-sortby.ts
+++ src/client/app/cnext/framework/model/q-cardview-sortby.ts
@@ -1,5 +1,5 @@
 export class QCardsortBy {
-  name:string='JobMatching';
-  listOfMatchings = ['BestMatch','Experience','Salary'];
-  sortValue = 'BestMatch';
+  name:string='Best match';
+  listOfMatchings = ['Best match','Experience','Salary'];
+  sortValue = 'Best match';
 }
