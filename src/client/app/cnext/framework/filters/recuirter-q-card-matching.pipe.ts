6285e679323c6aabd5df9525ce29492387eef305
kr!shna <ketan.sahane@techprimelab.com>
2017-05-29 08:37:35 +0530

    [cn-424]code refactor recuirter dashboard ->filter

--- src/client/app/cnext/framework/filters/recuirter-q-card-matching.pipe.ts
+++ src/client/app/cnext/framework/filters/recuirter-q-card-matching.pipe.ts
@@ -2,30 +2,32 @@ import {Pipe, PipeTransform} from "@angular/core";
 import {CandidateQCard} from "../model/candidateQcard";
 import {ValueConstant} from "../../../framework/shared/constants";
 
-@Pipe({name: 'candidatematching', pure: false})
+@Pipe({name: 'qcardmatching', pure: false})
 
 export class RecuirterQCardMatchingPipe implements PipeTransform {
 
-  transform(array: Array<CandidateQCard>, args: any, totalQCardMatches:any): Array<any> {
+  transform(array: Array<CandidateQCard>, args: any, totalQCardMatches:any,cartType:any): Array<any> {
 
     var defaultMatch = ValueConstant.VALUE_FOR_CNDIDATES_PERCENT_MATCHING_LOWER_BOUND;
     var result:CandidateQCard[];
     if (array == null) {
       return null;
     }
-    if (args == 'aboveMatch' || args == null) {
-      result = array.filter(item => ((item.exact_matching + item.above_one_step_matching) >= defaultMatch));
-      totalQCardMatches.count = result.length;
-      return result;
-    }
+    if(cartType =='matchedList') {
+      if (args == 'aboveMatch') {
+        result = array.filter(item => ((item.exact_matching + item.above_one_step_matching) >= defaultMatch));
+        totalQCardMatches.count = result.length;
+        return result;
+      }
 
-    if (args == 'belowMatch') {
-      result = array.filter(item => ((item.below_one_step_matching
-      + item.above_one_step_matching + item.exact_matching) >= defaultMatch));
-      totalQCardMatches.count = result.length;
-      return result;
+      if (args == 'belowMatch') {
+        //result = array.filter(item => ((item.below_one_step_matching + item.above_one_step_matching + item.exact_matching) >= defaultMatch));
+        result = array.filter(item => ((item.matching) >= defaultMatch));
+        totalQCardMatches.count = result.length;
+        return result;
+      }
     }
-
+    totalQCardMatches.count = array.length;
     return array;
   }
 }
