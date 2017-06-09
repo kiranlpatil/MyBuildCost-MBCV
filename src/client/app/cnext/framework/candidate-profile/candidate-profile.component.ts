7c2398f5327b18e9d400335824ab8fe3c9c386fc
ShrikantPatki <shrikant.patki@techprimelab.com>
2017-06-09 21:41:04 +0530

    settimeout bug

--- src/client/app/cnext/framework/candidate-profile/candidate-profile.component.ts
+++ src/client/app/cnext/framework/candidate-profile/candidate-profile.component.ts
@@ -41,6 +41,7 @@ export class CandidateProfileComponent implements OnInit {
   private candidateForCapability:Role[];
   private candidateForComplexity:Role[];
   private showTooltip:boolean = false;
+  private setTimeoutId : any;
   private showModalStyle:boolean = false;
   private goto:boolean = false;
   private highlightedSection:Section = new Section();
@@ -403,14 +404,19 @@ export class CandidateProfileComponent implements OnInit {
   }
 
   onSubmit() {
-    this.showModalStyle = !this.showModalStyle
+    this.showModalStyle = !this.showModalStyle;
+    if(this.setTimeoutId!==undefined) {
+      clearTimeout(this.setTimeoutId);
+    }
     this._router.navigate([NavigationRoutes.APP_CANDIDATE_DASHBOARD]);
   }
 
 
   showHideModal() {
     this.showModalStyle = !this.showModalStyle;
-    setInterval(() => this.onSubmit(), 1000 * 11);
+    this.setTimeoutId = setTimeout(()=> {
+      this.onSubmit();
+    }, 1000 * 11);
   }
 
   getStyleModal() {
