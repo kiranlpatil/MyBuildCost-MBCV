e90293a27c084c386a1e737c2b9f50d3b5d7e23b
root <krishna.ghatul@techprimelab.com>
2017-07-28 22:06:22 +0530

    [CN-939] profile comparison view

--- src/client/app/cnext/framework/profile-comparison/profile-comparison.service.ts
+++ src/client/app/cnext/framework/profile-comparison/profile-comparison.service.ts
@@ -17,7 +17,7 @@ export class ProfileComparisonService extends BaseService {
   getCompareDetail(candidateId: string[], jobId: string): Observable<any> {
     var id=  LocalStorageService.getLocalValue(LocalStorage.USER_ID);
     //var candidateId = ['5979825b64aa3dae0dfb46f6', '5979845064aa3dae0dfb47d3', '597a25bd64aa3dae0dfb4c2f', '597b1ef912bfacf80de44fc6', '597b1f421354eb2308db58cc'];
-    var url = 'recruiter' + '/' + id + '/' + 'jobprofile' + '/' + '597981a064aa3dae0dfb463d' + '?candidateId=' + JSON.stringify(candidateId);
+    var url = 'recruiter' + '/' + id + '/' + 'jobprofile' + '/' + jobId + '?candidateId=' + JSON.stringify(candidateId);
     return this.http.get(url)
       .map(this.extractData)
       .catch(this.handleError);
