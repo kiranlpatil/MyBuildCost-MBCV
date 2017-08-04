33965217bc96789d7ca29517d3fa7cd2eb0ab8e9
techprimelab <swapnil.nakhate@techprimelab.com>
2017-08-04 17:12:02 +0530

    [CN-1056]Emails should go from zoho mails "notifications@jobmosis.com".

--- src/client/app/framework/shared/constants.ts
+++ src/client/app/framework/shared/constants.ts
@@ -1,10 +1,10 @@
 export class AppSettings {
-  public static IP = 'http://localhost:8080';
+ // public static IP = 'http://localhost:8080';
   // public static IP = 'http://ee802b7f.ngrok.io';
   //public static IP = 'http://10.192.33.77:8080';
   //public static IP = 'http://52.89.221.23';
   // public static IP = 'app.jobmosis.com';
- // public static IP = '';
+  public static IP = '';
 
 
   public static get API_ENDPOINT(): string {
