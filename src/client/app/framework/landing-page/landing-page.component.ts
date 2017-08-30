d616718daf663f3038b944b6814a73fda199c228
Rahul Tiwari <shubhamtakode@gmail.com>
2017-08-30 19:53:39 +0530

    CN-970: Alert message on non chrome browser

--- src/client/app/framework/landing-page/landing-page.component.ts
+++ src/client/app/framework/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {ImagePath} from "../shared/index";
 import {Messages} from "../shared/constants";
 
@@ -8,11 +8,21 @@ import {Messages} from "../shared/constants";
   templateUrl: 'landing-page.component.html',
   styleUrls: ['landing-page.component.css'],
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
   BODY_BACKGROUND: string;
   landingPageText: string= Messages.MSG_LANDING_PAGE;
+  isChrome: boolean;
 
   constructor() {
     this.BODY_BACKGROUND = ImagePath.BODY_BACKGROUND;
+  }
+
+  ngOnInit() {
+     this.isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    console.log('isChrome = ', this.isChrome);
+  }
+
+  closeToaster() {
+     document.getElementById("snackbar").style.visibility = "hidden";
   }
 }
