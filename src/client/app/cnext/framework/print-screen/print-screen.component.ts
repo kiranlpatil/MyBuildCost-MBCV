65e2668111f1888587045541c35fa6218fb63db1
krishna <krish.ghatul007@gmail.com>
2017-09-27 19:31:43 +0530

    [CN-836] create pdf or img option provided

--- src/client/app/cnext/framework/print-screen/print-screen.component.ts
+++ src/client/app/cnext/framework/print-screen/print-screen.component.ts
@@ -17,23 +17,24 @@ export class PrintScreenComponent {
 
   }
 
-  createFile() {
+  createFile(_value:string) {
     if (this.screenIdForPrint !== '' && this.screenIdForPrint !== undefined) {
-      //var a = document.getElementById(this.screenIdForPrint)
       html2canvas(document.getElementById(this.screenIdForPrint))
         .then((canvas:any) => {
           var dataURL = canvas.toDataURL("image/jpeg");
-          this.fileToDownload.nativeElement.href = dataURL;
-          this.fileToDownload.nativeElement.download = 'xyzabc.jpeg';
-          //this.imageToDownload.nativeElement.click();
-          //doc.setFontSize(40);
-          //doc.text(35, 25, "Octonyan loves jsPDF");
-          var doc = new jsPDF("p", "mm", "a4");
-          var width = doc.internal.pageSize.width;
-          var height = doc.internal.pageSize.height;
-          doc.addImage(dataURL, 'JPEG', 0, 0, width, height);
-          doc.save('two-by-four.pdf')
-
+          if (_value == 'pdf') {
+            var doc = new jsPDF("p", "mm", "a4");
+            var width = doc.internal.pageSize.width;
+            var height = doc.internal.pageSize.height;
+            doc.addImage(dataURL, 'JPEG', 0, 0, width, height);
+            doc.save('two-by-four.pdf')
+          } else {
+            this.fileToDownload.nativeElement.href = dataURL;
+            this.fileToDownload.nativeElement.download = 'xyzabc.jpeg';
+            this.fileToDownload.nativeElement.click();
+            //doc.setFontSize(40);
+            //doc.text(35, 25, "Octonyan loves jsPDF");
+          }
         })
         .catch((err:any) => {
           console.log("error canvas", err);
