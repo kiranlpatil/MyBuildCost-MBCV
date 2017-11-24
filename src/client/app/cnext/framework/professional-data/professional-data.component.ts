533fe542c685fd82bb5b2fe83d455f687ade6919
shrikant <shrikant.patki@techprimelab.com>
2017-11-24 23:36:26 +0530

    [CN-1923] career plugin mail sending task

--- src/client/app/cnext/framework/professional-data/professional-data.component.ts
+++ src/client/app/cnext/framework/professional-data/professional-data.component.ts
@@ -145,7 +145,7 @@ export class ProfessionalDataComponent extends BaseService implements OnChanges,
   }
 
   onNext() {
-    if (this.candidate.professionalDetails.currentSalary ||
+    if (isNaN(this.candidate.professionalDetails.currentSalary) ||
       this.candidate.professionalDetails.industryExposure == '' || this.candidate.professionalDetails.noticePeriod == ''
       || this.candidate.professionalDetails.relocate == '') {
       this.isValid = false;
@@ -160,7 +160,7 @@ export class ProfessionalDataComponent extends BaseService implements OnChanges,
   }
 
   onSave() {
-    if (this.candidate.professionalDetails.currentSalary ||
+    if (isNaN(this.candidate.professionalDetails.currentSalary) ||
       this.candidate.professionalDetails.industryExposure == '' || this.candidate.professionalDetails.noticePeriod == ''
       || this.candidate.professionalDetails.relocate == '') {
       this.isValid = false;
