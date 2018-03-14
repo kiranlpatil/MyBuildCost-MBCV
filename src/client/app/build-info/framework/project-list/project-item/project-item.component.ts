61dedefdfadd1bf88177f2dd8003911981c827fd
Swapnil <swapnil.nakhate1010@gmail.com>
2018-03-14 14:53:09 +0530

    [BUILDINFO] TSLint Build error

--- src/client/app/build-info/framework/project-list/project-item/project-item.component.ts
+++ src/client/app/build-info/framework/project-list/project-item/project-item.component.ts
@@ -7,8 +7,7 @@ import { Project } from '../../model/project';
 @Component({
   moduleId: module.id,
   selector: 'bi-project-item',
-  templateUrl: 'project-item.component.html',
- /* styleUrls: ['project-item.component.scss']*/
+  templateUrl: 'project-item.component.html'
 })
 
 export class ProjectItemComponent {
