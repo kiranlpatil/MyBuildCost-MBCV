bfa0132b83a9e17e8d56d636a16f1452659db171
Swapnil <swapnil.nakhate1010@gmail.com>
2018-04-13 10:00:55 +0530

    [BUILDINFO-563] dependency issue :manual typings file deleted

--- tools/manual_typings/project/jspdf2.d.ts
+++
@@ -1,9 +0,0 @@
-import { jsPDF } from './jspdf';
-
-interface jsPdfPlug {
-  fromHTML(HTML: string | HTMLElement, x: number, y: number, settings?: any, callback?: Function, margins?: any): jsPDF;
-
-  save(filename: string): jsPDF;
-}
-
-declare var jsPdfPlug: jsPdfPlug;
