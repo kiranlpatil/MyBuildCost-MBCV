c8ef9e5295f974160bd23a44c049e5621f566e1f
Dhanashri123 <dhanashri.patil@techprimelab.com>
2018-05-26 13:04:30 +0530

    [BUILDINFO-782]models backend

--- src/server/app/applicationProject/dataaccess/model/project/building/SteelQuantityItem.ts
+++ src/server/app/applicationProject/dataaccess/model/project/building/SteelQuantityItem.ts
@@ -1,15 +1,8 @@
 class SteelQuantityItem {
   item: string;
-  diameterOfBar : number;
-  lengthOfBar : number;
-  nosOfBar: number;
-  weightOf6mm : number;
-  weightOf8mm : number;
-  weightOf10mm : number;
-  weightOf12mm : number;
-  weightOf16mm : number;
-  weightOf20mm : number;
-  weightOf25mm : number;
-  weightOf30mm : number;
+  diameter : number;
+  length : number;
+  nos: number;
+  weight : number;
 }
 export = SteelQuantityItem;
