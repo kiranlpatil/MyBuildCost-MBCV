701560c479b18679f5fdbace3daa5b8327fedc4e
Anil <anil.gadge@techprimelab.com>
2018-08-06 13:12:25 +0530

    [BI]Automation

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,7 +1,7 @@
 sudo kill `sudo lsof -t -i:8080`
 cd /home/ubuntu/apps/costcontrol-automation/dist/
 sudo mkdir -p logs
-sudo chown -R ubuntu /home/ubuntu/apps/costcontrol-automation
+sudo chown -R ubuntu /home/ubuntu/apps
 sudo npm -v
 sudo npm install
 sudo node app.server.prod.js --NODE_ENV=staging &> /dev/null 2> /dev/null < /dev/null &
