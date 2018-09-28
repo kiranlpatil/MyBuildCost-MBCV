567196a815aa038d6058bd735427dd5e7ac5625c
Swapnil <swapnil.nakhate1010@gmail.com>
2018-09-28 20:00:58 +0530

    [BUILDINFO-1103] Automated Build Process using Pipeline for Production artifacts - Test-Automation [Reviewed by Bhushan]

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,7 +1,7 @@
 sudo kill `sudo lsof -t -i:8080`
-cd /home/ubuntu/apps/costcontrol-automation/dist/
+cd /home/ubuntu/apps/test-costcontrol-automation/dist/
 mkdir -p logs
-chown -R ubuntu /home/ubuntu/apps/costcontrol-automation
+chown -R ubuntu /home/ubuntu/apps/test-costcontrol-automation
 sudo npm -v
 sudo node -v
 npm -v
