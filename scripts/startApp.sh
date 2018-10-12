e89e5a6c573ce9feae1b53e259c52225c2fcc61d
Swapnil <swapnil.nakhate1010@gmail.com>
2018-10-12 10:50:13 +0530

    [BUILDINFO-168] MBC Production automation -Test 2

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,10 +1,10 @@
-sudo kill `sudo lsof -t -i:8080`
-cd /home/ubuntu/apps/costcontrol-prod-automation
+sudo kill `sudo lsof -t -i:80`
+cd /home/indrajeetmalave/CostControl/costcontrol-prod-automation
 curl -O -L -o myBuildCost https://bitbucket.org/tplabs/costcontrol/downloads/RA_Module_Production_Artifact.zip -u 'AnilGadge:Anil$1993'
 sudo apt-get update
 sudo apt-get install -y zip
 unzip RA_Module_Production_Artifact.zip
-chown -R ubuntu /home/ubuntu/apps/costcontrol-prod-automation
+chown -R ubuntu /home/indrajeetmalave/CostControl/costcontrol-prod-automation
 sudo rm -rf RA_Module_Production_Artifact.zip
 mkdir -p logs
 sudo npm -v
