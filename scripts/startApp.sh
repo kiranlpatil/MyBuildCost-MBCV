0c4e68f824a346af0cff15c3d5d0f9091dec5b5c
Swapnil <swapnil.nakhate1010@gmail.com>
2018-10-12 14:05:46 +0530

    [BUILDINFO-1103] MBC Production automation - Final bash script Testing

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,8 +1,9 @@
 sudo kill `sudo lsof -t -i:80`
-curl -O -L -o myBuildCost https://bitbucket.org/tplabs/costcontrol/downloads/RA_Module_Production_Artifact.zip -u 'AnilGadge:Anil$1993'
+curl -O -L -o myBuildCost https://bitbucket.org/tplabs/costcontrol/downloads/MBC_ProdArtifacts.zip -u 'AnilGadge:Anil$1993'
 sudo apt-get update
-unzip -v
 unzip MBC_ProdArtifacts.zip
 npm -v
 node -v
-sudo npm install --unsafe -perm
+sudo npm install
+mkdir -p logs
+node app.server.prod.js --NODE_ENV=production> /dev/null 2> /dev/null < /dev/null &
