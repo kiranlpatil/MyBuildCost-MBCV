a2a3237d72584253b779fac3430d9c22948668fe
swapnil133 <swapnil.kadam@techprimelab.com>
2019-07-04 10:12:11 +0530

    [BUILDINFO-1332] New home page UI

---
+++ src/client/app/framework/home-page/home-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ImagePath, NavigationRoutes } from '../../shared/index';
+import { Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'home-page',
+  templateUrl: 'home-page.component.html',
+  styleUrls: ['home-page.component.css'],
+})
+
+export class HomePageComponent {
+
+}
