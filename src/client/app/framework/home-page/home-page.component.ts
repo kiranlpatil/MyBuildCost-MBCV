544f23fc0a74bb129195ff59998dadfaeda18089
swapnil133 <swapnil.kadam@techprimelab.com>
2019-07-10 16:45:04 +0530

    [BUILDINFO-1322] Adds new video

--- src/client/app/framework/home-page/home-page.component.ts
+++ src/client/app/framework/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ImagePath, NavigationRoutes } from '../../shared/index';
 import { Router } from '@angular/router';
+
+declare let $: any;
 
 @Component({
   moduleId: module.id,
@@ -9,14 +11,22 @@ import { Router } from '@angular/router';
   styleUrls: ['home-page.component.css'],
 })
 
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
   scrollToSection($element: any) {
     $element.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest"
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
     });
   }
 
+  ngOnInit() {
+    $('#video-modal').on('hidden.bs.modal',() => {
+      $('video').trigger('pause');
+    });
+    $('#video-modal').on('show.bs.modal',() => {
+      $('video').trigger('play');
+    });
+  }
 }
