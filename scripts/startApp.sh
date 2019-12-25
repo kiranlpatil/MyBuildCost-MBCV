1b900af78662a00a971cd707b296349f06173972
Anil <anil.gadge@techprimelab.com>
2019-12-25 12:24:22 +0530

    [BUILDINFO-1452]Autobuild

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,9 +1,12 @@
 sudo kill `sudo lsof -t -i:8080`
-cd /home/ubuntu/apps/costcontrol-automation/dist/
-mkdir -p logs
 chown -R ubuntu /home/ubuntu/apps/costcontrol-automation
+curl -O -L -o myBuildCost https://bitbucket.org/tplabs/costcontrol/downloads/MBC_StageArtifacts.zip -u 'AnilGadge:tpl1234567890'
+sudo apt-get update
+unzip MBC_StageArtifacts.zip
+sudo rm -rf MBC_StageArtifacts.zip
+cd dist
 sudo npm -v
 sudo node -v
-npm -v
-node -v
+mkdir -p logs
+sudo npm install --unsafe -perm
 node app.server.prod2.js --NODE_ENV=staging &> /dev/null 2> /dev/null < /dev/null &
